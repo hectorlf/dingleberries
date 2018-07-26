a5f771b9fc4a19fbda9b9768b13de72514987306
hectorlf <hectorlopezfernandez@hotmail.com>
2018-07-26 12:48:29 +0200

    Implement a more vertx-standard startup

--- src/main/java/com/hectorlopezfernandez/dingleberries/MainVerticle.java
+++ src/main/java/com/hectorlopezfernandez/dingleberries/MainVerticle.java
@@ -1,11 +1,6 @@
 package com.hectorlopezfernandez.dingleberries;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
@@ -17,16 +12,6 @@ public class MainVerticle extends AbstractVerticle {
     		routingContext.response().putHeader("content-type", "text/html").end("Hello World!");
     	});
     	vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-        System.out.println("HTTP server started on port 8080");
     }
-
-    public static void main(String[] args) throws InterruptedException {
-    	BlockingQueue<AsyncResult<String>> q = new ArrayBlockingQueue<>(1);
-    	Vertx.vertx().deployVerticle(new MainVerticle(), q::offer);
-    	AsyncResult<String> result = q.take();
-    	if (result.failed()) {
-    		throw new RuntimeException(result.cause());
-    	}
-	}
 
 }
